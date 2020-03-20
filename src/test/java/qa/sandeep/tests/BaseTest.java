d87ce049a995ad89b976ae124c845b3f6e38de42
Sandeep Thukral <sandeep.thukral@insided.com>
2020-03-20 23:38:44 +0100

    utils are now instantiated in BaseTest

--- src/test/java/qa/sandeep/tests/BaseTest.java
+++ src/test/java/qa/sandeep/tests/BaseTest.java
@@ -1,7 +1,9 @@
 package qa.sandeep.tests;
 
 import qa.sandeep.GistClient;
+import qa.sandeep.utils.RandomUtils;
 
 public class BaseTest {
     GistClient gistClient = new GistClient();
+    RandomUtils utils = new RandomUtils();
 }
